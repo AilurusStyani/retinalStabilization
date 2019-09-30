64842f02376ca01981267cc0a57b1b7871cc0386
softdrinks <boyce_bao@163.com>
2019-09-30 16:26:36 +0800

    V5.13
    plot two direction in one graph

--- analyzer/Psychophysical/analyzer.m
+++ analyzer/Psychophysical/analyzer.m
@@ -1,11 +1,11 @@
 close all;
 clear all;
 
-filePath = 'D:\BYC\2019Intern\2019Internship\Dhwani\retinalStabilization\stimulus\data';
-edfFile = dir(fullfile(filePath,'*.edf'));
+filePath = 'D:\BYC\2019Intern\2019Internship\Dhwani\retinalStabilization\stimulus\data\004';
+file = dir(fullfile(filePath,'retinalStabilization_*.mat'));
 flipNameStr = 'flip';
-pBias = nan(4,length(edfFile));
-pThreshold = nan(4,length(edfFile));
+pBias = nan(4,length(file));
+pThreshold = nan(4,length(file));
 popChoice = [];
 pI = 0;
 % hPopulation = tight_subplot(2,2,[0.5 0.5]);
@@ -31,15 +31,15 @@ popPursuitDir{4} = [];
 popFixSpe{4} = [];
 
 pChoice = cell(4,1);
-
+colorIndex = {[0.8 0.2 0.2],[0.2 0.8 0.2]};
 figureNum = 1;
-for i = 1:length(edfFile)
-    matFile = strrep(edfFile(i).name,'.edf','.mat');
-    data = load(fullfile(filePath,matFile));
+for i = 1:length(file)
+    fileName = file(i).name;
+    data = load(fullfile(filePath,fileName));
     popChoice = cat(1,popChoice,data.choice);
     trialLength = length(data.Conditions);
-    flipDegree = contains(matFile,flipNameStr);
-    num = regexp(matFile, '(\d+)','tokens');
+    flipDegree = contains(fileName,flipNameStr);
+    num = regexp(fileName, '(\d+)','tokens');
     subNum = cell2mat(num{1});
     
     % motion type 1:
@@ -68,154 +68,153 @@ for i = 1:length(edfFile)
     
     
     
-    for j = 1:trialLength
+    for triali = 1:trialLength
         
-        if isempty(data.Conditions{j})
+        if isempty(data.Conditions{triali})
             continue
         else
-            motionType = data.Conditions{j}(end);
+            motionType = data.Conditions{triali}(end);
         end
         
         if motionType == 1
-            headDeg{1}(j,:) = [data.Conditions{j}(2),j];
-            headSpe{1}(j,:) = [data.Conditions{j}(3),j];
+            headDeg{1}(triali,:) = [data.Conditions{triali}(2),triali];
+            headSpe{1}(triali,:) = [data.Conditions{triali}(3),triali];
         elseif motionType == 2
-            headDeg{2}(j,:) = [data.Conditions{j}(2),j];
-            headSpe{2}(j,:) = [data.Conditions{j}(3),j];
-            pursuitDir{2}(j,:) = [data.Conditions{j}(4),j];
-            fixSpe{2}(j,:) = [data.Conditions{j}(5),j];
+            headDeg{2}(triali,:) = [data.Conditions{triali}(2),triali];
+            headSpe{2}(triali,:) = [data.Conditions{triali}(3),triali];
+            pursuitDir{2}(triali,:) = [data.Conditions{triali}(4),triali];
+            fixSpe{2}(triali,:) = [data.Conditions{triali}(5),triali];
         elseif motionType == 3
-            headDeg{3}(j,:) = [data.Conditions{j}(2),j];
-            headSpe{3}(j,:) = [data.Conditions{j}(3),j];
-            pursuitDir{3}(j,:) = [data.Conditions{j}(4),j];
-            rotationSpe{3}(j,:) = [data.Conditions{j}(5),j];
+            headDeg{3}(triali,:) = [data.Conditions{triali}(2),triali];
+            headSpe{3}(triali,:) = [data.Conditions{triali}(3),triali];
+            pursuitDir{3}(triali,:) = [data.Conditions{triali}(4),triali];
+            rotationSpe{3}(triali,:) = [data.Conditions{triali}(5),triali];
         elseif motionType == 4
-            headDeg{4}(j,:) = [data.Conditions{j}(2),j];
-            headSpe{4}(j,:) = [data.Conditions{j}(3),j];
-            pursuitDir{4}(j,:) = [data.Conditions{j}(4),j];
-            fixSpe{4}(j,:) = [data.Conditions{j}(5),j];
+            headDeg{4}(triali,:) = [data.Conditions{triali}(2),triali];
+            headSpe{4}(triali,:) = [data.Conditions{triali}(3),triali];
+            pursuitDir{4}(triali,:) = [data.Conditions{triali}(4),triali];
+            fixSpe{4}(triali,:) = [data.Conditions{triali}(5),triali];
         end
     end
     
     uniqueDeg = cell(1,4);
-    for j = 1:4
-        del = isnan(headDeg{j}(:,1));
-        headDeg{j}(del,:) = [];
+    for typei = 1:4
+        del = isnan(headDeg{typei}(:,1));
+        headDeg{typei}(del,:) = [];
         
         if flipDegree
-            headDeg{j}(:,1) = -headDeg{j}(:,1);
+            headDeg{typei}(:,1) = -headDeg{typei}(:,1);
         end
         
-        del = isnan(headSpe{j}(:,1));
-        headSpe{j}(del,:) = [];
-        if j == 2 || j == 4
-            del = isnan(pursuitDir{j}(:,1));
-            pursuitDir{j}(del,:) = [];
-            del = isnan(fixSpe{j}(:,1));
-            fixSpe{j}(del,:) = [];
+        del = isnan(headSpe{typei}(:,1));
+        headSpe{typei}(del,:) = [];
+        if typei == 2 || typei == 4
+            del = isnan(pursuitDir{typei}(:,1));
+            pursuitDir{typei}(del,:) = [];
+            del = isnan(fixSpe{typei}(:,1));
+            fixSpe{typei}(del,:) = [];
             
-            popPursuitDir{j} = cat(1,popPursuitDir{j},pursuitDir{j});
-            popFixSpe{j} = cat(1,popFixSpe{j},fixSpe{j});
-        elseif j ==3
-            del = isnan(pursuitDir{j}(:,1));
-            pursuitDir{j}(del,:) = [];
-            del = isnan(rotationSpe{j}(:,1));
-            rotationSpe{j}(del,:) = [];
+            popPursuitDir{typei} = cat(1,popPursuitDir{typei},pursuitDir{typei});
+            popFixSpe{typei} = cat(1,popFixSpe{typei},fixSpe{typei});
+        elseif typei ==3
+            del = isnan(pursuitDir{typei}(:,1));
+            pursuitDir{typei}(del,:) = [];
+            del = isnan(rotationSpe{typei}(:,1));
+            rotationSpe{typei}(del,:) = [];
             
-            popPursuitDir{j} = cat(1,popPursuitDir{j},pursuitDir{j});
-            popRotationSpe{j} = cat(1,popRotationSpe{j},rotationSpe{j});
+            popPursuitDir{typei} = cat(1,popPursuitDir{typei},pursuitDir{typei});
+            popRotationSpe{typei} = cat(1,popRotationSpe{typei},rotationSpe{typei});
         end
-        uniqueDeg{j} = unique(headDeg{j}(:,1));
+        uniqueDeg{typei} = unique(headDeg{typei}(:,1));
         
-        popHeadDeg{j} = cat(1,popHeadDeg{j},headDeg{j});
-        popHeadSpe{j} = cat(1,popHeadSpe{j},headSpe{j});
-        pChoice{j} = cat(1,pChoice{j},data.choice(headDeg{j}(:,2)));
+        popHeadDeg{typei} = cat(1,popHeadDeg{typei},headDeg{typei});
+        popHeadSpe{typei} = cat(1,popHeadSpe{typei},headSpe{typei});
+        pChoice{typei} = cat(1,pChoice{typei},data.choice(headDeg{typei}(:,2)));
     end
     
     % [~,hSubplot1] = tight_subplot(1,4,[0.02 0.02]);
-    for j=1:4
-        choice = cell(length(uniqueDeg{j}),1);
-        pRight = cell(length(uniqueDeg{j}),1);
-        choiceTime = cell(length(uniqueDeg{j}),1);
+    for typei=1:4
+        choice = cell(length(uniqueDeg{typei}),1);
+        pRight = cell(length(uniqueDeg{typei}),1);
+        choiceTime = cell(length(uniqueDeg{typei}),1);
         
-        if j > 1
-            uniqueDir = unique(pursuitDir{j}(:,1));
+        if typei > 1
+            uniqueDir = unique(pursuitDir{typei}(:,1));
             dirLength = length(uniqueDir);
-            for dir = 1:dirLength
-                trialIndexI = find(pursuitDir{j}(:,1) == uniqueDir(dir));
-                dirIndex{dir} = pursuitDir{j}(trialIndexI,:);
+            for directioni = 1:dirLength
+                trialIndexI = find(pursuitDir{typei}(:,1) == uniqueDir(directioni));
+                dirIndex{directioni} = pursuitDir{typei}(trialIndexI,:);
             end
         end
         
-        if j == 1
-            for k = 1:length(uniqueDeg{j})
-                index = ismember(headDeg{j}(:,1),uniqueDeg{j}(k));
-                trialIndex = headDeg{j}(index,2);
+        if typei == 1
+            for k = 1:length(uniqueDeg{typei})
+                index = ismember(headDeg{typei}(:,1),uniqueDeg{typei}(k));
+                trialIndex = headDeg{typei}(index,2);
                 choice{k} = data.choice(trialIndex); %%
                 pRight{k} = sum(choice{k} == 2)./ length(choice{k});
                 choiceTime{k} = length(choice{k});
             end
-            fitData = [uniqueDeg{j},cell2mat(pRight),cell2mat(choiceTime)];
+            fitData = [uniqueDeg{typei},cell2mat(pRight),cell2mat(choiceTime)];
             
             [bias,threshold] = cum_gaussfit_max1(fitData(1:end,:));
-            pBias(j,i) = bias;
-            pThreshold(j,i) = threshold;
-            xi = min(uniqueDeg{j}):0.1:max(uniqueDeg{j});
+            pBias(typei,i) = bias;
+            pThreshold(typei,i) = threshold;
+            xi = min(uniqueDeg{typei}):0.1:max(uniqueDeg{typei});
             y_fit = cum_gaussfit([bias,threshold],xi);
             
             if ishandle(figureNum); end; figure(figureNum); set(gcf,'color','white');
             plot([90,90],[0,1],'-.k');
             hold on
-            plot(uniqueDeg{j},cell2mat(pRight),'*');
-            plot(xi,y_fit,'-');
+            plot(uniqueDeg{typei},cell2mat(pRight),'*k');
+            plot(xi,y_fit,'-k');
             set(gca, 'xlim',[-15,15],'ylim',[0 1])
             xlabel('Heading degree');
             ylabel('Proportion of "right" choice');
-            title(['Condition ' num2str(j) ' for participant ' subNum]);
+            title(['Condition ' num2str(typei) ' for participant ' subNum]);
             %     hleg1=legend('choice','mean & standard error','linear result');
             %     set(hleg1,'Location','EastOutside')
-            text(5,0.8,sprintf('\\it\\mu_{psy} = \\rm%6.3g\\circ',bias),'color','b')
-            text(5,0.7,sprintf('\\it\\sigma_{psy} = \\rm%6.3g\\circ', threshold),'color','b')
-            figureNum = figureNum+1;
-        elseif j>1
-            for dir = 1:dirLength
-                for k = 1:length(uniqueDeg{j})
-                    index = find(ismember(headDeg{j}(:,1),uniqueDeg{j}(k)));
-                    trialIndex = headDeg{j}(index,2);
-                    trialIndex = trialIndex(ismember(trialIndex,dirIndex{dir}(:,2)));
+            text(5,0.8,sprintf('\\it\\mu_{psy} = \\rm%6.3g\\circ',bias),'color','k')
+            text(5,0.7,sprintf('\\it\\sigma_{psy} = \\rm%6.3g\\circ', threshold),'color','k')
+        elseif typei>1
+            for directioni = 1:dirLength
+                for k = 1:length(uniqueDeg{typei})
+                    index = find(ismember(headDeg{typei}(:,1),uniqueDeg{typei}(k)));
+                    trialIndex = headDeg{typei}(index,2);
+                    trialIndex = trialIndex(ismember(trialIndex,dirIndex{directioni}(:,2)));
                     choice{k} = data.choice(trialIndex); %%
                     pRight{k} = sum(choice{k} == 2)./ length(choice{k});
                     choiceTime{k} = length(choice{k});
                     
                 end
-                fitData = [uniqueDeg{j},cell2mat(pRight),cell2mat(choiceTime)];
+                fitData = [uniqueDeg{typei},cell2mat(pRight),cell2mat(choiceTime)];
                 if min(cell2mat(pRight)) == 1
                     fitData(:,2) = fitData(:,2) - 0.01;
                 elseif max(cell2mat(pRight)) == 0
                     fitData(:,2) = fitData(:,2) + 0.01;
                 end
                 [bias,threshold] = cum_gaussfit_max1(fitData(1:end,:));
-                pBias(j,i) = bias;
-                pThreshold(j,i) = threshold;
-                xi = min(uniqueDeg{j}):0.1:max(uniqueDeg{j});
+                pBias(typei,i) = bias;
+                pThreshold(typei,i) = threshold;
+                xi = min(uniqueDeg{typei}):0.1:max(uniqueDeg{typei});
                 y_fit = cum_gaussfit([bias,threshold],xi);
                 
                 if ishandle(figureNum); end; figure(figureNum); set(gcf,'color','white');
                 plot([90,90],[0,1],'-.k');
                 hold on
-                plot(uniqueDeg{j},cell2mat(pRight),'*');
-                plot(xi,y_fit,'-');
+                plot(uniqueDeg{typei},cell2mat(pRight),'*','color',colorIndex{directioni});
+                plot(xi,y_fit,'-','color',colorIndex{directioni});
                 set(gca, 'xlim',[-15,15],'ylim',[0 1])
                 xlabel('Heading degree');
                 ylabel('Proportion of "right" choice');
-                title(['Condition ' num2str(j) ' direction ' num2str(dir) ' for participant ' subNum]);
+                title(['Condition ' num2str(typei) ' for participant ' subNum]);
                 %     hleg1=legend('choice','mean & standard error','linear result');
                 %     set(hleg1,'Location','EastOutside')
-                text(5,0.8,sprintf('\\it\\mu_{psy} = \\rm%6.3g\\circ',bias),'color','b')
-                text(5,0.7,sprintf('\\it\\sigma_{psy} = \\rm%6.3g\\circ', threshold),'color','b')
-                figureNum = figureNum+1;
+                text(5,1-0.2*directioni,sprintf('\\it\\mu_{psy} = \\rm%6.3g\\circ',bias),'color',colorIndex{directioni})
+                text(5,0.9-0.2*directioni,sprintf('\\it\\sigma_{psy} = \\rm%6.3g\\circ', threshold),'color',colorIndex{directioni})
             end
         end
+        figureNum = figureNum+1;
     end
 end
 
